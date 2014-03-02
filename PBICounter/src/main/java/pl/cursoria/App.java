7eac588513eb2efe40f6c6e5a878df53c9ac8edd
Kamil <Koodziej>
2014-03-02 22:14:26 +0100

    First Commit

---
+++ PBICounter/src/main/java/pl/cursoria/App.java
@@ -0,0 +1,13 @@
+package pl.cursoria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
